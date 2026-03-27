c7314ddb7b3417f95c5ce8022ce2853356078df0
AlongkornFR <144777531+AlongkornFR@users.noreply.github.com>
2026-03-27 06:16:57 +0100

    Update page.js

--- frontend/src/app/cart/page.js
+++ frontend/src/app/cart/page.js
@@ -3,8 +3,8 @@
 import Link from "next/link";
 import { useCart } from "@/components/context/CartContext";
 import { DEFAULT_PRODUCT_IMAGE } from "@/utils/constant";
-import { FaXmark, FaMinus, FaPlus, FaShoppingBag } from "react-icons/fa6";
-import { FaShieldAlt, FaMapMarkerAlt } from "react-icons/fa";
+import { FaXmark, FaShoppingBag } from "react-icons/fa6";
+import { FaShieldAlt, FaMapMarkerAlt, FaMinus, FaPlus } from "react-icons/fa";
 
 function formatEUR(value) {
   return (Number(value) || 0).toLocaleString("fr-FR", { style: "currency", currency: "EUR" });
